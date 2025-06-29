a1ae79e24f75a600388a706f175e94f70bcb6ccb
Orlov Oleg <61846414+orlov-oleg-developer@users.noreply.github.com>
2025-06-29 21:53:13 +0300

    feat: [header] test_1

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,15 @@
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './pages/App/ui/App';
+import { init, miniApp } from '@telegram-apps/sdk-react';
 
+// Инициализация SDK
+init();
+
+// Монтируем miniApp синхронно
+if (miniApp.mountSync.isAvailable()) {
+  miniApp.mountSync();
+}
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <App />
 )
